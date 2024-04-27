bfa76969933589861db3832ff6b0a5771e448be7
Rajkumar Yadav <online.rajkumaryadav@gmail.com>
2024-04-27 17:08:32 +0530

    added controller functionality

---
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -0,0 +1,38 @@
+package com.springboot.blog.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.service.PostService;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+	
+	private PostService postService;
+
+	public PostController(PostService postService) {
+//		super();
+		this.postService = postService;
+	}
+	
+//	create blog post rest api
+	@PostMapping
+	public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
+		return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+	}
+	
+	@GetMapping
+	public List<PostDto> getAllPosts() {
+		return postService.getAllPosts();
+	}
+	
+}
