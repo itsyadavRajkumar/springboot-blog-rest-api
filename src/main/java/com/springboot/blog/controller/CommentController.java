d346b65df76599bc4e5ef7b9a7e4810923e08eba
Rajkumar Yadav <online.rajkumaryadav@gmail.com>
2024-04-30 00:13:27 +0530

    created Update API for comment update

--- src/main/java/com/springboot/blog/controller/CommentController.java
+++ src/main/java/com/springboot/blog/controller/CommentController.java
@@ -7,8 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.springboot.blog.payload.CommentDto;
 import com.springboot.blog.service.CommentService;
@@ -38,5 +40,11 @@ public class CommentController {
 	public ResponseEntity<CommentDto> getCommentById(@PathVariable(value = "postId") Long postId, @PathVariable(value = "id") Long commentId) {
 		CommentDto commentDto = commentService.getCommentById(postId, commentId);
 		return new ResponseEntity<>(commentDto, HttpStatus.OK);
+	}
+	
+	@PutMapping("/posts/{postId}/comments/{id}")
+	public ResponseEntity<CommentDto> updateComment(@PathVariable(value = "postId") Long postId, @PathVariable(value = "id") Long commentId, @RequestBody CommentDto commentDto) {
+		CommentDto updatedComment = commentService.updateComment(postId, commentId, commentDto);
+		return new ResponseEntity<>(updatedComment, HttpStatus.OK);
 	}
 }
