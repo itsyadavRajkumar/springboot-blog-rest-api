e550e8f0c2c8ede0136ad4ba3514db692a87872e
Rajkumar Yadav <online.rajkumaryadav@gmail.com>
2024-05-03 18:54:17 +0530

    added @valid annotation to configuration with @Bean

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -88,7 +88,7 @@ public class PostController {
 	
 //	update post by id rest api
 	@PutMapping("/{id}")
-	public ResponseEntity<PostDto> updatePost(@RequestBody PostDto postDto, @PathVariable(name = "id") long id) {
+	public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto, @PathVariable(name = "id") long id) {
 		PostDto postResponce = postService.updatePost(postDto, id);
 		return new ResponseEntity<>(postResponce, HttpStatus.OK);
 	}
