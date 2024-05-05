67bf5d6e13f94e34f71e2a2c11244a9bfb6bbca3
Rajkumar Yadav <online.rajkumaryadav@gmail.com>
2024-05-05 12:43:35 +0530

    Securing REST API's with In-memory Authentication

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -2,6 +2,7 @@ package com.springboot.blog.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -37,6 +38,7 @@ public class PostController {
 		return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
 	}
 	*/
+	@PreAuthorize("hasRole('ADMIN')")
 	@PostMapping
 	public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto) {
 		return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
@@ -87,6 +89,7 @@ public class PostController {
 	}
 	
 //	update post by id rest api
+	@PreAuthorize("hasRole('ADMIN')")
 	@PutMapping("/{id}")
 	public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto, @PathVariable(name = "id") long id) {
 		PostDto postResponce = postService.updatePost(postDto, id);
@@ -94,6 +97,7 @@ public class PostController {
 	}
 	
 //	delete post by id rest api
+	@PreAuthorize("hasRole('ADMIN')")
 	@DeleteMapping("/{id}")
 	public ResponseEntity<String> deletePost(@PathVariable(name = "id") long id) {
 		postService.deletePostById(id);
