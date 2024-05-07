976b9e3ff5194abde0f0207be8eb02cf0d7f4fc1
Rajkumar Yadav <online.rajkumaryadav@gmail.com>
2024-05-07 16:06:54 +0530

    Created Spring data JPA Repositories - UserRepository and RoleRepository

--- src/main/java/com/springboot/blog/entity/User.java
+++ src/main/java/com/springboot/blog/entity/User.java
@@ -25,7 +25,7 @@ import lombok.Setter;
 @Entity
 @Table(name = "users")
 public class User {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -34,14 +34,11 @@ public class User {
 	private String username;
 	@Column(nullable = false, unique = true)
 	private String email;
-	@Column(nullable = false	)
+	@Column(nullable = false)
 	private String password;
-	
+
 	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-	@JoinTable(name = "user_roles",
-	joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
-	inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
-			)
+	@JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
 	private Set<Role> roles;
-	
+
 }
