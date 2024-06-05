7620b7a971b88489fa74ce3421cfcdbcbb8d82cb
Rajkumar Yadav <online.rajkumaryadav@gmail.com>
2024-06-06 00:05:10 +0530

    unnecessary removed comments

--- src/main/java/com/springboot/blog/entity/Post.java
+++ src/main/java/com/springboot/blog/entity/Post.java
@@ -40,47 +40,7 @@ public class Post {
 	@Column(name = "content", nullable = false)
 	private String content;
 
-//	public Long getId() {
-//		return id;
-//	}
-//
-//	public void setId(Long id) {
-//		this.id = id;
-//	}
-//
-//	public String getTitle() {
-//		return title;
-//	}
-//
-//	public void setTitle(String title) {
-//		this.title = title;
-//	}
-//
-//	public String getDescription() {
-//		return description;
-//	}
-//
-//	public void setDescription(String description) {
-//		this.description = description;
-//	}
-//
-//	public String getContent() {
-//		return content;
-//	}
-//
-//	public void setContent(String content) {
-//		this.content = content;
-//	}
-
 	@OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
 	private Set<Comment> comments = new HashSet<>();
-
-//	public Set<Comment> getComments() {
-//		return comments;
-//	}
-//
-//	public void setComments(Set<Comment> comments) {
-//		this.comments = comments;
-//	}
 
 }
